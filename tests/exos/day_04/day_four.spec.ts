8590a260fb621a43f94b8c9a64fb6116b3e01f5d
aland404 <duchene.alan56@gmail.com>
2020-12-13 19:04:41 +0100

    Init day four

---
+++ tests/exos/day_04/day_four.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+// import { } from '../../../src/exos/day_04/helperDayFour'
+import 'mocha'
+
+const input = [
+]
+
+describe('day four part 1', () => {
+  it('', () => {
+  })
+})
+
+describe('day four part 2', () => {
+  it('', () => {
+  })
+})
